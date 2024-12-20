4556bbeb6afbdd360f90254b742ac4631048a39a
codelady-ship <jalilova.antiqa@gmail.com>
2024-12-20 17:40:14 +0400

    javascript task

---
+++ task6string.js
@@ -0,0 +1,35 @@
+/*1.İstifadəçidən ikirəqəmli ədəd daxil etməsini xahiş edin. Bu rəqəmi üçrəqəmli olana qədər, 7 artırın. Son dəyəri konsola çıxarın.*/
+
+/*2.Konsola N dəfə «I know how to use cycles» mesajı çıxaran proqram yazın.
+Proqram N ədədini istifadəçidən soruşur.*/
+
+
+/*3.İstifadəçidən tam adı «AD SOYAD» formatında daxil etməyi xahiş edin. Tam adını konsola fərqli qaydada yəni «SOYAD AD» formatında çıxarın,*/
+
+/*4.Telefon nömrəsini parametr kimi qəbul edən ısvalidnumber() funksiyasını yazın. Telefon nömrəsi 11 rəqəmdən ibarət olduqda və +7 ilə başladıqda, funksiya true qaytarır.*/
+
+
+/*5.Funksiya yazın.Arqument olaraq aldığı sözün içərisində rəqəmləri seçsin və geri qaytarsın.
+Məsələn: "hell5o wor6ld" -> 56
+getNumberFromString(number)*/
+
+/*6.Remove all exclamation
+Funksiya yazın.Arqument olaraq string alsın,sözün içərisindəki bütün nida işarələrini silsin və sözün sonuna bir ədəd nida işarəsi əlavə etsin.
+məsələn:
+He!llo! !!FE-25 // netice olmalıdır Hello FE-25!*/
+
+/*7.Funksiya yazın,qəbul etdiyi parametrin baş hərfini böyütsün və ekarana yazdırsın*/
+/*8.Funksiya yazın,qəbul etdiyi parametrin bütün hərflərini böyütsün və ekarana yazdırsın*/
+/*9.Funksiya yazın,qəbul etdiyi parametrin bütün hərflərini kiçiltsin və ekarana yazdırsın*/
+
+/*10.Funksiya yaradin, ilk characterimizi deyisek: ilkHerfiDeyis(soz, ilkHerf)
+ilkHerfiDeyis('Qara', "Y") // Yara*/
+
+/*11. funksiya yazın ad və soyad alsın və aşağıdakı kimi nəticə versin
+nameFormat(str) —> Ramin Mammadzada-=> M. Ramin*/
+
+
+/*12. verify gmail address. (gmail addressleri sonu @gmail.com ile bitmelidir, abcdf@gmail.com meselen) //true*/
+/*13. Funksiya yazın,aldığı paramterdə bütün hərflər böyükdürsə true deyilsə false qaytarsın*/
+/*14. Funksiya yazın.Cümlə alacaq parametr olaraq.Funksiya isə cümlənin ilk 5 simvolunu geri qaytaracaq*/
+/*15. Funksiya yazın.Parametr alsın.Sözün içərisində nəqədər boşluq varsa hamsı - işarəsi olsun*/
